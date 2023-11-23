87ddee19c006881618450fb1d09007f2fc4feb1b
dato201 <146935809+dato201@users.noreply.github.com>
2023-11-23 21:20:40 +0400

    add lesson2 dan

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+let color = ["Blue ", "Green", "Red", "Orange", "Violet", "Indigo", "Yellow ", ];
+let  o = ["th","st","nd","rd"]
+
+console.log(typeof color);
+
+console.log(color[0]);
+
+console.log(color[color.length - 1]);
+
+
+
+console.log(1+ o[1]+ " choice is "+ color[0]);
+console.log(2+ o[2]+ " choice is "+ color[1]);
+console.log(3+ o[3]+ " choice is "+ color[2]);
+console.log(4+ o[0]+ " choice is "+ color[3]);
+console.log(5+ o[0]+ " choice is "+ color[4]);
+console.log(6+ o[0]+ " choice is "+ color[5]);
+console.log(7+ o[0]+ " choice is "+ color[6]);
+
+let student = {
+    name: "dato",
+    lastName: "balxamishvili",
+    asaki: 3128391729371389
+};
+
+student.age = 19;
+
+delete student.asaki
+
+console.log(student);
+ 
+ 
+ 
